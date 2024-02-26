c9e4c465a4c88e6f7bf352b58801dd9538a3d4f9
Amaury Franssen <amauryfranssen@gmail.com>
2024-02-26 22:20:52 +0100

    feat(sections): meilleure organisation des sections et couleurs verte ajoutée

--- credit.php
+++ credit.php
@@ -2,4 +2,5 @@
 <a href="https://fr.freepik.com/vecteurs-libre/personnes-tenant-icones-arbres-pour-sensibiliser-environnement_3226144.htm#query=values&position=3&from_view=search&track=sph&uuid=fd1964ad-c29d-4247-ae02-0efcdb00a9b1#position=3&query=values">Image de rawpixel.com</a> sur Freepik
 <a href="https://fr.freepik.com/vecteurs-libre/employes-donnant-mains-aidant-leurs-collegues-monter-escaliers_7732609.htm#query=valeurs&position=1&from_view=keyword&track=sph&uuid=a6d33d0f-07d6-45b1-af8e-fc67a23f27c5">Image de pch.vector</a> sur Freepik
 <!-- Persons -->
-<a href="https://fr.freepik.com/search?format=search&freeSvg=free&last_filter=freeSvg&last_value=free&query=person&type=icon">Icône de UIcons</a> 
+<a href="https://fr.freepik.com/search?format=search&freeSvg=free&last_filter=freeSvg&last_value=free&query=person&type=icon">Icône de UIcons</a>
+<a href="https://fr.freepik.com/icone/utilisateur_3917711#fromView=search&page=1&position=52&uuid=44b56f94-79f0-48c2-9fed-3fe8f55c6976">Icône de UIcons</a>
