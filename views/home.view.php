7e1ba9e96c96574ad14b130ecf55b55bc0f3a522
Amaury Franssen <amauryfranssen@gmail.com>
2024-04-01 17:18:07 +0200

    feat(home): change phone number to obfuscate the good one later

--- views/home.view.php
+++ views/home.view.php
@@ -317,6 +317,9 @@ $partners = [
         <div class="container">
             <div class="row mb-5 col-12">
                 <h2 class="pre-title pre-title--centered">Nous&nbsp;contacter</h2>
+                <div class="my-3">
+                    <p class="els-text-lg els-text--secondary els-text--centered">Si vous voulez vous engager avec nous, nous serons très heureux de vous accueillir: <br/> contactez-nous par email ou téléphone. </p>
+                </div>
             </div>
 
             <div class="row">
@@ -328,8 +331,9 @@ $partners = [
                         <div class="contact-container__email">
                             <div class="">
                                 <span class="pb-3 contact-address els-text els-text-lg">Basé à Tsévié, à 35km de Lomé.</span>
-                                <span class="pb-3 contact-schedules els-text els-text-lg">Horaires d’ouverture : <br /> lun-jeu de 9-12h et de 14-17h. Ven- de 9-12h</span>
-                                <span class="pb-3 contact-tel els-text els-text-lg">(+228) 93 58 04 18</span>
+                                <span class="pb-3 contact-schedules els-text els-text-lg">Horaires d’ouverture : <br /> Nous n'avons pas d'horaires fixes. N'hésitez pas à nous laisser un message, nous vous rappellerons.</span>
+                                <!--  need to be obfusctated -->
+                                <a href="tel:+00000000000" class="pb-3 contact-tel els-text els-text-lg">(+000) 00 00 00 00</a>
                             </div>
                             <div class="contact-email-text els-text els-text-lg">
                                 <a
